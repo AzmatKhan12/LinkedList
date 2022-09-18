2daba4290a2e4df7ed7cc0071d9401297fcad398
AzmatKhan12 <rehan.dgms@gmail.com>
2022-09-18 19:12:59 +0530

    print method, prepend & append

--- linkedList.js
+++ linkedList.js
@@ -15,9 +15,70 @@ class LinkedList {
     getSize() {
         return this.size;
     }
+
+    // Add the node at first position
+    prepend(value) {
+        const node = new Node(value);
+        // first condition
+        if (this.isEmpty()) {
+            this.head = node;
+        }
+        //2nd condition
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    //print method
+    print() {
+        if (this.isEmpty()) {
+            console.log('List is Empty');
+        }
+        else {
+            let curr = this.head;
+            let listValue = '';
+            while (curr) {
+                listValue += `${curr.value}` + ' ';
+                curr = curr.next;
+            }
+            console.log(listValue);
+
+        }
+
+    }
+
+    //Add new node at the End
+    append(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+        }
+        else {
+            let prev = this.head;
+            while (prev.next) {
+                prev = prev.next;
+            }
+            prev.next = node
+        }
+        this.size++;
+    }
 }
 
 let list = new LinkedList();
 console.log(list.isEmpty());
 console.log(list.getSize());
+
+list.prepend(10);
+list.print();
+
+list.prepend(20);
+list.prepend(30);
+list.print();
+
+list.append(40);
+list.print();
+
+
 
